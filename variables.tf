30aca8d37e8567be7da3ca4039d3e18eeedfb00f
Elvis R <elvikito@webeando.me>
2023-05-02 13:27:11 -0400

    first commit

---
+++ variables.tf
@@ -0,0 +1,49 @@
+variable "resource_group" {
+  description = "The name of your Azure Resource Group."
+  default     = "rg-azure"
+}
+
+variable "prefix" {
+  description = "This prefix will be included in the name of some resources."
+  default     = "tftest"
+}
+
+variable "hostname" {
+  description = "Virtual machine hostname. Used for local hostname, DNS, and storage-related names."
+  default     = "erapp"
+}
+
+variable "location" {
+  description = "The region where the virtual network is created."
+  default     = "canadacentral"
+}
+
+variable "vm_size" {
+  description = "Specifies the size of the virtual machine."
+  default     = "Standard_A0"
+}
+
+variable "image_publisher" {
+  description = "Name of the publisher of the image (az vm image list)"
+  default     = "Canonical"
+}
+
+variable "image_offer" {
+  description = "Name of the offer (az vm image list)"
+  default     = "UbuntuServer"
+}
+
+variable "image_sku" {
+  description = "Image SKU to apply (az vm image list)"
+  default     = "20.04-LTS"
+}
+
+variable "admin_username" {
+  description = "Administrator user name"
+  default     = "adminuser"
+}
+
+variable "admin_password" {
+  description = "Administrator password"
+  default     = "Adminpassword123!"
+}
