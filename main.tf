00f93bf0387032b51b0c69f894ef4ca7ea5e475d
Elvis <elvikito@gmail.com>
2023-05-02 17:59:03 +0000

    Updated

--- main.tf
+++ main.tf
@@ -19,10 +19,6 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   address_space       = ["10.0.0.0/16"]
   resource_group_name = azurerm_resource_group.rg.name
-
-  tags = {
-    Environment = "Novacene"
-  }
 }
 
 # Subnet
@@ -30,10 +26,7 @@ resource "azurerm_subnet" "subnet" {
   name                 = "${var.prefix}-subnet"
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
-  address_prefix       = "10.0.10.0/24"
-  tags = {
-    Environment = "Novacene"
-  }
+  address_prefixes     = ["10.0.1.0/24"]
 }
 
 # Network security group
@@ -41,7 +34,6 @@ resource "azurerm_network_security_group" "sg" {
   name                = "${var.prefix}-sg"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
-  description         = "Security group for ${var.prefix}"
 
   security_rule {
     name                       = "HTTP"
@@ -69,7 +61,7 @@ resource "azurerm_network_security_group" "sg" {
 
   security_rule {
     name                       = "HTTPS"
-    priority                   = 101
+    priority                   = 102
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
@@ -81,35 +73,34 @@ resource "azurerm_network_security_group" "sg" {
 }
 
 # Network interface
-resource "azurerm_network_interface" "tf-guide-nic" {
-  name                      = "${var.prefix}tf-guide-nic"
-  location                  = var.location
-  resource_group_name       = azurerm_resource_group.tf_azure_guide.name
-  network_security_group_id = azurerm_network_security_group.tf-guide-sg.id
+resource "azurerm_network_interface" "tf-nic" {
+  name                = "${var.prefix}tf-nic"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "${var.prefix}ipconfig"
+    name                          = "${var.prefix}-ip-config"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.tf-guide-pip.id
+    public_ip_address_id          = azurerm_public_ip.tf-pip.id
   }
 }
 
-resource "azurerm_public_ip" "tf-guide-pip" {
+resource "azurerm_public_ip" "tf-pip" {
   name                         = "${var.prefix}-ip"
-  location                     = "${var.location}"
-  resource_group_name          = "${azurerm_resource_group.tf_azure_guide.name}"
-  public_ip_address_allocation = "Dynamic"
-  domain_name_label            = "${var.hostname}"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = var.location
+  allocation_method            = "Static"
+  sku                          = "Standard"
 }
 
 resource "azurerm_virtual_machine" "site" {
   name                = "${var.hostname}-site"
   location            = var.location
-  resource_group_name = azurerm_resource_group.tf_azure_guide.name
+  resource_group_name = azurerm_resource_group.rg.name
   vm_size             = var.vm_size
 
-  network_interface_ids         = [azurerm_network_interface.tf-guide-nic.id]
+  network_interface_ids         = [azurerm_network_interface.tf-nic.id]
   delete_os_disk_on_termination = true
 
   storage_image_reference {
